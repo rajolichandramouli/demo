3f3f8a517cb4187d2aa175d79b4436d67c7dfabe
rajolichandramouli <52004809+rajolichandramouli@users.noreply.github.com>
2019-06-19 21:34:12 +0530

    Add files via upload

---
+++ AbstractClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterviewPractice
+{
+    class AbstractClass:A1
+    {
+        public static void Main()
+        {
+            AbstractClass obj = new AbstractClass();
+            obj.Add();
+            obj.AbstractMethode();
+            Console.ReadLine();
+            //Can not create the instance for Abstract class and Interfaces.
+            //A1 obj = new A1();
+            
+        }
+        internal override void AbstractMethode()
+        {
+            Console.WriteLine("Abstract Methode");
+        }
+    }
+    abstract class A1
+    {
+        static A1()
+        {
+        }
+        public void Add()
+        {
+            Console.WriteLine("Non-Abstract Methodes");
+        }
+        internal abstract void AbstractMethode();
+        //{
+           // Console.WriteLine("Abstract Methode");
+        //}
+    }
+    abstract class A2
+    {
+        public void Add()
+        {
+            Console.WriteLine("Non-Abstract Methodes");
+        }
+        public abstract void AbstractMethode();
+        //{
+        // Console.WriteLine("Abstract Methode");
+        //}
+    }
+}
